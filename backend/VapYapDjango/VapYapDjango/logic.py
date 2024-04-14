6c4605e550ceb953ee4ceb0a906dd931e926c3ff
grahamHeathcote <grahamkheathcote@gmail.com>
2024-04-14 23:07:37 +0100

    Fixed Kstejs shit

--- backend/VapYapDjango/VapYapDjango/logic.py
+++ backend/VapYapDjango/VapYapDjango/logic.py
@@ -6,34 +6,17 @@ client = openai.OpenAI(
     api_key=os.environ["OPENAI_API_KEY"]
 )
 
-<<<<<<< HEAD
 
-def returnValue(request: HttpRequest):
-    if 'chat_history' not in request.session:
-        request.session['chat_history'] = []
 
-    user_message = "Make me a poem"
-    request.session['chat_history'].append({"role": "user", "content": user_message})
-
-    # Call OpenAI API with the accumulated chat history
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=request.session['chat_history']
-    )
-
-    # Extract AI's response and save it to chat histrory
-    ai_response = response.choices[0].message["content"]
-    request.session['chat_history'].append({"role": "system", "content": ai_response})
-    request.session.modified = True
-=======
 
 
 def returnJSONObject(request: HttpRequest):
     user_message = "Write a poem"
     ai_response = makeAPIRequestFresh(user_message, request)
->>>>>>> origin/master
 
     return JsonResponse({"ai_response": ai_response})
+
+
 
 
 
