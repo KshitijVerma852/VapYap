d72e51610701c09aad56f9631e11ea294bcd97fc
grahamHeathcote <grahamkheathcote@gmail.com>
2024-04-14 23:28:03 +0100

    Added FreshSystem to logic

--- backend/VapYapDjango/VapYapDjango/logic.py
+++ backend/VapYapDjango/VapYapDjango/logic.py
@@ -11,21 +11,34 @@ client = openai.OpenAI(
 
 
 def returnJSONObject(request: HttpRequest):
+    system_message = "Please speak to me in Spanish onkly"
     user_message = "Write a poem"
-    ai_response = makeAPIRequestFresh(user_message, request)
+    ai_response = makeAPIRequestFreshSystem(system_message, user_message, request)
 
     return JsonResponse({"ai_response": ai_response})
 
 
 
 
+def makeAPIRequestFreshSystem(systemMessage, user_message, request):
+    request.session['chat_history'] = []
+    request.session['chat_history'].append({"role": "system", "content": systemMessage})
+    request.session['chat_history'].append({"role": "user", "content": user_message})
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=request.session['chat_history']
+    )
+    ai_response = response.choices[0].message.content
+    request.session['chat_history'].append({"role": "system", "content": ai_response})
+    request.session.modified = True
+    return ai_response
 
 
 def makeAPIRequestFresh(user_message, request):
     request.session['chat_history'] = []
     request.session['chat_history'].append({"role": "user", "content": user_message})
     response = client.chat.completions.create(
-        model="gpt-4-turbo",
+        model="gpt-3-turbo",
         messages=request.session['chat_history']
     )
     ai_response = response.choices[0].message.content
