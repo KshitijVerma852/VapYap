7095d9ff8d88da4bf4b0c7df9c7618eef3812500
grahamHeathcote <grahamkheathcote@gmail.com>
2024-04-16 22:42:20 +0100

    Added answering basic capabilites

--- backend/VapYapDjango/VapYapDjango/logic.py
+++ backend/VapYapDjango/VapYapDjango/logic.py
@@ -6,7 +6,6 @@ client = openai.OpenAI(
     api_key=os.environ["OPENAI_API_KEY"]
 )
 
-
 def makeAPIRequestFreshSystem(systemMessage, user_message):
     chatgptData = {
         "session": {
@@ -17,6 +16,21 @@ def makeAPIRequestFreshSystem(systemMessage, user_message):
     chatgptData["session"]['chat_history'].append({"role": "user", "content": user_message})
     response = client.chat.completions.create(
         model="gpt-4-turbo",
+        messages=chatgptData["session"]['chat_history']
+    )
+    ai_response = response.choices[0].message.content
+    return ai_response
+
+def makeAPIRequestFreshSystemTurbo(systemMessage, user_message):
+    chatgptData = {
+        "session": {
+            "chat_history": []
+        }
+    }
+    chatgptData["session"]['chat_history'].append({"role": "system", "content": systemMessage})
+    chatgptData["session"]['chat_history'].append({"role": "user", "content": user_message})
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
         messages=chatgptData["session"]['chat_history']
     )
     ai_response = response.choices[0].message.content
--- backend/VapYapDjango/VapYapDjango/parsing.py
+++ backend/VapYapDjango/VapYapDjango/parsing.py
@@ -2,19 +2,20 @@ import json
 import os
 from django.http import JsonResponse, HttpRequest
 from .logic import makeAPIRequestFreshSystem
+from .logic import makeAPIRequestFreshSystemTurbo
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def returnJSONObject(request: HttpRequest):
-
+    print("Start running")
     motion = "This house believes that religion does more harm than good"
     infoSlide = ""
     position = "OG"
     parse_RawArguments(rawDebateInput, rawDebateOutput)
     clean_RawArguments(rawDebateOutput, cleanDebateOutput)
+    answerArguments(cleanDebateOutput, answerDebateOutput)
     caseGeneration(motion, infoSlide, position)
-        
     return JsonResponse({"ai_response": "dfdai_response"})
 
 def caseGeneration(motion, infoSlide, position):
@@ -25,8 +26,7 @@ def caseGeneration(motion, infoSlide, position):
     brainStormedIdeas = makeAPIRequestFreshSystem(brainStormMessage, speechDetails)
     with open(BrainStormOutput, 'w') as file:
         file.write(brainStormedIdeas)
-
-    
+    print(f"Brainstorming has been written to {BrainStormMessageFile}")
 
 def clean_RawArguments(input_filename, output_filename):
     with open(input_filename, 'r') as file:
@@ -36,7 +36,7 @@ def clean_RawArguments(input_filename, output_filename):
     clean_data = {}
     for speech_type, arguments in data.items():
         clean_data[speech_type] = [{
-            'text': makeAPIRequestFreshSystem(cleanMessage, arg['text']),
+            'text': makeAPIRequestFreshSystemTurbo(cleanMessage, arg['text']),
             'strength': arg['strength']
         } for arg in arguments]
 
@@ -45,6 +45,22 @@ def clean_RawArguments(input_filename, output_filename):
 
     print(f"Clean data has been written to {output_filename}")
 
+def answerArguments(input_filename, output_filename):
+    with open(input_filename, 'r') as file:
+        data = json.load(file)
+    with open(answerMessageFile, 'r') as file:
+        answerMessage = file.read()
+    clean_data = {}
+    for speech_type, arguments in data.items():
+        clean_data[speech_type] = [{
+            'text': makeAPIRequestFreshSystem(answerMessage, arg['text']),
+            'strength': arg['strength']
+        } for arg in arguments]
+
+    with open(output_filename, 'w') as file:
+        json.dump(clean_data, file, indent=4)
+
+    print(f"Answered data has been written to {output_filename}")
 
 def parse_RawArguments(input_filename, output_filename):
     debate_data = {}
@@ -82,11 +98,12 @@ def parse_RawArguments(input_filename, output_filename):
 
     print(f"Data has been written to {output_filename}")
 
-
 rawDebateInput = os.getcwd() + '/VapYapDjango/content/speech.txt'
 rawDebateOutput = os.getcwd() + '/VapYapDjango/content/RawTracking.json'
 cleanDebateOutput = os.getcwd() + '/VapYapDjango/content/CleanTracking.json'
+answerDebateOutput = os.getcwd() + '/VapYapDjango/content/AnswerTracking.json'
 BrainStormOutput = os.getcwd() + '/VapYapDjango/content/BrainStorm.txt'
 
 cleanMessageFile = os.getcwd() + '/VapYapDjango/prompts/argumentCleaning.txt'
 BrainStormMessageFile = os.getcwd() + '/VapYapDjango/prompts/motionBrainStorm.txt'
+answerMessageFile = os.getcwd() + '/VapYapDjango/prompts/argumentAnswer.txt'
