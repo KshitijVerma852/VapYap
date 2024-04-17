115c9b2202da181339775656b9562abbc86e6f77
grahamHeathcote <grahamkheathcote@gmail.com>
2024-04-17 01:24:20 +0100

    Added caseGen to parsing

--- backend/VapYapDjango/VapYapDjango/parsing.py
+++ backend/VapYapDjango/VapYapDjango/parsing.py
@@ -9,25 +9,36 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def returnJSONObject(request: HttpRequest):
     print("Start running")
-    motion = "This house believes that religion does more harm than good"
+    motion ="This House believes that democratic states should grant an amnesty to whistleblowers who expose unethical practices in the government."
     infoSlide = ""
     position = "OG"
     parse_RawArguments(rawDebateInput, rawDebateOutput)
     clean_RawArguments(rawDebateOutput, cleanDebateOutput)
-    answerArguments(cleanDebateOutput, answerDebateOutput)
+    #answerArguments(cleanDebateOutput, answerDebateOutput)
     caseGeneration(motion, infoSlide, position)
     return JsonResponse({"ai_response": "dfdai_response"})
 
-
 def caseGeneration(motion, infoSlide, position):
-    speechDetails = (
-            "The motion you need to brainstorm reads: " + motion + " The info slide, if it exists reads: " + infoSlide + "You are to think of arguments for side :" + position)
+    
+    speechDetails = ("The motion you need to brainstorm reads: " +motion + " The info slide, if it exists reads: " + infoSlide + "You are to think of arguments for side :" + position)
     with open(BrainStormMessageFile, 'r') as file:
         brainStormMessage = file.read()
     brainStormedIdeas = makeAPIRequestFreshSystem(brainStormMessage, speechDetails)
     with open(BrainStormOutput, 'w') as file:
         file.write(brainStormedIdeas)
     print(f"Brainstorming has been written to {BrainStormMessageFile}")
+
+    with open(PMCaseGeneration, 'r') as file:
+        PMMessage = file.read()
+    
+    debateInfo = (speechDetails + "My ideas are below" + brainStormedIdeas)
+
+    PM = makeAPIRequestFreshSystem(PMMessage, debateInfo)
+
+
+    with open(PMOutput, 'w') as file:
+        file.write(PM)
+    print(f"Case has been written to {PMOutput}")
 
 
 def clean_RawArguments(input_filename, output_filename):
@@ -47,7 +58,6 @@ def clean_RawArguments(input_filename, output_filename):
 
     print(f"Clean data has been written to {output_filename}")
 
-
 def answerArguments(input_filename, output_filename):
     with open(input_filename, 'r') as file:
         data = json.load(file)
@@ -65,9 +75,7 @@ def answerArguments(input_filename, output_filename):
 
     print(f"Answered data has been written to {output_filename}")
 
-
 def parse_RawArguments(input_filename, output_filename):
-    print("Parsing")
     debate_data = {}
     try:
         with open(output_filename, 'r') as file:
@@ -84,26 +92,15 @@ def parse_RawArguments(input_filename, output_filename):
     arguments = debate_data.get(speech_type, [])
 
     for line in lines[1:]:
-        rawArgumentType = line[0]
-        line = line[2:]
         line = line.strip().replace(' /n', '')
         last_space_index = line.rfind(' ')
         argument_text = line[:last_space_index].strip()
         argument_strength = line[last_space_index:].strip()
 
-        argumentType = ""
-        if rawArgumentType == "w":
-            argumentType = "weighing"
-        elif rawArgumentType == "a":
-            argumentType = "answer"
-        elif rawArgumentType == "c":
-            argumentType = "case"
-
         if not any(arg['text'] == argument_text for arg in arguments):
             arguments.append({
                 'text': argument_text,
-                'strength': int(argument_strength),
-                'type': argumentType
+                'strength': int(argument_strength)
             })
 
     debate_data[speech_type] = arguments
@@ -114,13 +111,16 @@ def parse_RawArguments(input_filename, output_filename):
 
     print(f"Data has been written to {output_filename}")
 
-
 rawDebateInput = os.getcwd() + '/VapYapDjango/content/speech.txt'
 rawDebateOutput = os.getcwd() + '/VapYapDjango/content/RawTracking.json'
 cleanDebateOutput = os.getcwd() + '/VapYapDjango/content/CleanTracking.json'
 answerDebateOutput = os.getcwd() + '/VapYapDjango/content/AnswerTracking.json'
 BrainStormOutput = os.getcwd() + '/VapYapDjango/content/BrainStorm.txt'
+PMOutput = os.getcwd() + '/VapYapDjango/content/PMCase.txt'
+
+
 
 cleanMessageFile = os.getcwd() + '/VapYapDjango/prompts/argumentCleaning.txt'
 BrainStormMessageFile = os.getcwd() + '/VapYapDjango/prompts/motionBrainStorm.txt'
 answerMessageFile = os.getcwd() + '/VapYapDjango/prompts/argumentAnswer.txt'
+PMCaseGeneration = os.getcwd() + '/VapYapDjango/prompts/caseGen/PMCaseGeneration.txt'
