c61e5505bf9b077e95359cf1cdcf27e47e2aace1
grahamkheathcote <grahamkheathcote@gmail.com>
2024-04-28 15:17:49 +0100

    Tried changing static stuff

--- backend/VapYapDjango/VapYapDjango/settings.py
+++ backend/VapYapDjango/VapYapDjango/settings.py
@@ -5,9 +5,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'oruewhfolureg bogdgdffdgdfgdfgogagdsgsdfg!!!!'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1',]
+
+
 
 
 # Application definition
@@ -95,8 +97,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
