aaab5daee9621fe94761ff894613ff317582d561
grahamkheathcote <grahamkheathcote@gmail.com>
2024-04-28 11:16:48 +0100

    Added static file thing ??

--- backend/VapYapDjango/VapYapDjango/settings.py
+++ backend/VapYapDjango/VapYapDjango/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,3 +98,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
