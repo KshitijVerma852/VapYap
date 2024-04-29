3e33dfafc71fe3578a0bd410dfdceed89187ae42
grahamkheathcote <grahamkheathcote@gmail.com>
2024-04-29 06:42:40 +0100

    Hoping it works

--- backend/VapYapDjango/VapYapDjango/wsgi.py
+++ backend/VapYapDjango/VapYapDjango/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VapYapDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VapYapDjango.VapYapDjango.settings')
 
 application = get_wsgi_application()
