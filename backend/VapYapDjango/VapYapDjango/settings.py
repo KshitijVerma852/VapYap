11f21fbb94212a71ae0fc169885ba3acf0330d3a
grahamkheathcote <grahamkheathcote@gmail.com>
2024-04-29 02:34:18 +0100

    added hindi guy

--- backend/VapYapDjango/VapYapDjango/settings.py
+++ backend/VapYapDjango/VapYapDjango/settings.py
@@ -46,7 +46,9 @@ ROOT_URLCONF = 'VapYapDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
