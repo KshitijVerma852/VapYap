669d54408f8a9756f7afc65a38fea4b559843b15
kv45 <kshitijverma852@gmail.com>
2024-05-22 04:21:15 +0100

    move the frontend inside VapYapDjango

--- backend/VapYapDjango/VapYapDjango/settings.py
+++ backend/VapYapDjango/VapYapDjango/settings.py
@@ -100,4 +100,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
+]
